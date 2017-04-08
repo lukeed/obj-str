858da2d1bb82f6c46d2581564bce71c2af43d361
Luke Edwards <luke.edwards05@gmail.com>
2017-04-08 16:05:59 -0700

    add tests

--- test/index.js
+++ test/index.js
@@ -1,6 +1,19 @@
 const test = require('tape');
-const fn = require('../');
+const fn = require('../dist/obj-str');
 
-test('title', t => {
-	t.is(fn('unicorns'), 'unicorns & rainbows');
+const one = true;
+const two = true;
+const bad = false;
+
+test('obj-str', t => {
+	t.equal(typeof fn, 'function', 'exports a function');
+
+	t.equal(fn(), '', 'returns empty string by default');
+	t.equal(fn({}), '', `{} --> ''`);
+	t.equal(fn({foo: true}), 'foo', `{ foo:true } --> foo`);
+	t.equal(fn({foo: true, bar: false}), 'foo', `{ foo:true, bar:false } --> foo`);
+	t.equal(fn({foo: 1 === 1, bar: 1 !== 1, baz: 1 !== 2}), 'foo baz', `{ foo:1===1, bar:1!==1, baz:1!==2 } --> foo baz`);
+	t.equal(fn({one, two, bad}), 'one two', `{ one, two, bad } --> one two`);
+
+	t.end();
 });
