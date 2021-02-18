56b9c4ffa2b3806e4cf45bec8a4a72569a844c22
Luke Edwards <luke.edwards05@gmail.com>
2021-02-18 10:51:05 -0800

    chore: update test runner

--- test/index.js
+++ test/index.js
@@ -1,22 +1,75 @@
-const test = require('tape');
-const fn = require('../dist/obj-str');
+import { suite } from 'uvu';
+import * as assert from 'uvu/assert';
+import objstr from '../src';
 
-const one = true;
-const two = true;
-const bad = false;
-const key = 'abc';
+function is(input, expect) {
+	assert.is(objstr(input), expect);
+}
 
-test('obj-str', t => {
-	t.equal(typeof fn, 'function', 'exports a function');
+const API = suite('exports');
 
-	t.equal(fn(), '', 'returns empty string by default');
-	t.equal(fn({}), '', `{} --> ''`);
-	t.equal(fn({foo: true}), 'foo', `{ foo:true } --> foo`);
-	t.equal(fn({foo: true, bar: false}), 'foo', `{ foo:true, bar:false } --> foo`);
-	t.equal(fn({foo: 1 === 1, bar: 1 !== 1, baz: 1 !== 2}), 'foo baz', `{ foo:1===1, bar:1!==1, baz:1!==2 } --> foo baz`);
-	t.equal(fn({one, two, bad}), 'one two', `{ one, two, bad } --> one two`);
-	t.equal(fn({'-foo': true}), '-foo', `{ '-foo':true } --> -foo`);
-	t.equal(fn({[key]: true}), 'abc', `{ [key]:true } --> abc`);
-
-	t.end();
+API('should export a function', () => {
+	assert.type(objstr, 'function');
 });
+
+API.run();
+
+// ---
+
+const usage = suite('usage');
+
+usage('true -> ""', () => {
+	is(true, '');
+});
+
+usage('false -> ""', () => {
+	is(false, '');
+});
+
+usage('undefined -> ""', () => {
+	is(undefined, '');
+});
+
+usage('null -> ""', () => {
+	is(null, '');
+});
+
+usage('{} -> ""', () => {
+	is({}, '');
+});
+
+usage('[] -> ""', () => {
+	is([], '');
+});
+
+usage('{ foo } -> "foo"', () => {
+	is({ foo: true }, 'foo');
+	is({ foo: 1 }, 'foo');
+});
+
+usage('{ foo, bar } -> "foo"', () => {
+	is({ foo: true, bar: false }, 'foo');
+	is({ foo: 1, bar: 0 }, 'foo');
+});
+
+usage('{ foo, bar, baz } -> "foo baz"', () => {
+	is({ foo: 1 === 1, bar: 1 !== 1, baz: 1 !== 2 }, 'foo baz');
+	is({ foo: assert, bar: null, baz: Date }, 'foo baz');
+});
+
+usage('{ one, two, bad } -> "one two"', () => {
+	let one=true, two=true, bad=false;
+	is({ one, two, bad }, 'one two');
+});
+
+usage('{ "-foo": x } -> "-foo"', () => {
+	is({ '-foo': true }, '-foo');
+	is({ '-foo': 0, '-foo': 1 }, '-foo');
+});
+
+usage('{ [key]: x } -> key', () => {
+	let key = 'abc';
+	is({ [key]: true }, 'abc');
+});
+
+usage.run();
